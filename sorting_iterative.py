7d7cd1a0e61c2836bb3abf56729633f5fb05ea3d
FaithKauwe <fkauwe@gmail.com>
2026-01-24 17:48:56 -0800

    is_sorted and bubble_sort completed, tests passing

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -3,18 +3,37 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    Time Complexity: O(n), its just determined by the length of the list, there are no nested loops
+    Space Complexity: O(1), jut a single variable i which is constant space, no matter list size. No new lists or data structures. """
+    # for nums: start at the beginning of list, index 0, eachtime through the loop. compare index i with index i+1, 
+    # if at any time the value of i+1 is smaller than i, return False and exit the loop.
+    # else complete the loop and return True. the for loop will automatically increase the value of i by 1 each time through the loop.
+    for i in range(len(items) - 1):
+        if items[i] > items[i + 1]:
+            return False
+    return True
+
 
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Time Complexity: O(n^2), its determined by the nested loops. The outer loop runs n times, and the inner loop runs n-1 times for each iteration of the outer loop.
+    Space Complexity: O(1), just the 2 variables swapped and i which are constant space, no matter list size. No new lists or data structures. """
+    # the boolean flag is used to check if the list is sorted
+    swapped = True
+    # the conditional that keeps the loop running
+    while swapped:
+        swapped = False
+        
+        for i in range(len(items) - 1):
+            # compare with the next item
+            if items[i + 1] < items[i]:
+                # swap the items
+                items[i], items[i + 1] = items[i + 1], items[i]
+                
+                # set the flag to True to continue the loop
+                swapped = True
 
 
 def selection_sort(items):
